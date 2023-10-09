57798e941867ad60da12eb2bed7e0067c014ed4c
Andreas Offenhaeuser <offenhaeuser@gmail.com>
2023-10-09 12:15:16 +0000

    add home

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,9 +1,7 @@
-import {
-  createHashRouter
-} from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import App from '../App';
-import Radar from '../components/radar'
-
+import Radar from '../components/radar';
+import Home from '../components/home';
 
 export const router = createHashRouter([
   {
@@ -13,6 +11,10 @@ export const router = createHashRouter([
       {
         path: "radar",
         element: <Radar />,
+      },
+      {
+        path: "/",
+        element: <Home />,
       },
     ],
   },
