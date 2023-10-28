81f4aa245f8a7c0391beb01ac7d8a2b404bd1a89
Andreas Offenhaeuser <offenhaeuser@gmail.com>
2023-10-28 13:55:10 +0000

    remove bootstrap stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { Outlet } from "react-router-dom";
 import './App.scss'
 import NavBar from './NavBar'
-import React from "react";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 function App() {
   return (
